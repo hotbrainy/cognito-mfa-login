fa41adc9c7823e7231364321d83324159578ea34
hotbrainy <brainhg95@gmail.com>
2023-01-24 21:12:37 -0500

    [fixed] index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { GlobalStyles } from './global';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 ReactDOM.render(
   <>
